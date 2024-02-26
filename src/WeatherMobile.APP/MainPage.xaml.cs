d8ee5b5937ac8d02564d786fe4d1eea8e0bfc449
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2024-02-26 10:54:39 -0300

    criação de projeto para services e services client

--- src/WeatherMobile.APP/MainPage.xaml.cs
+++ src/WeatherMobile.APP/MainPage.xaml.cs
@@ -1,74 +1,98 @@
 ﻿using WeatherMobile.APP.Model;
 using Newtonsoft.Json;
 
-namespace WeatherMobile.APP
+namespace WeatherMobile.APP;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    private readonly HttpClient _httpClient;
+
+    public MainPage()
     {
-        private readonly HttpClient _httpClient;
+        InitializeComponent();
+        _httpClient = new HttpClient();
+    }
 
-        public MainPage()
+    private async void GetWeatherButton_Clicked(object sender, EventArgs e)
+    {
+        string cidade = cidadeEntry.Text.Trim();
+        Weather weather = await RecuperarPrevisaoPorCidade(cidade);
+
+        if (weather != null)
         {
-            InitializeComponent();
-            _httpClient = new HttpClient();
+            temperaturaLabel.Text = "Temperatura: " + weather.Temperatura + "°C";
+            condicaoLabel.Text = "Condição: " + weather.Condicao;
+            umidadeLabel.Text = "Umidade: " + weather.Umidade;
+            velocidadeVentoLabel.Text = "Velocidade do Vento: " + weather.VelocidadeVento + " km/h";
         }
-
-        private async void GetWeatherButton_Clicked(object sender, EventArgs e)
+        else
         {
-            string cidade = cidadeEntry.Text.Trim();
-
-            Weather weatherData = await Teste(cidade);
-
-            Weather weather = new("Blumenau", "Santa Catarina", "Brasil", 25, 28, 22, "Alerta Laranja de chuvas", "Trovoadas", 82, 8);
-
-            if (weather != null)
-            {
-                temperaturaLabel.Text = "Temperatura: " + weather.Temperatura + "°C";
-                condicaoLabel.Text = "Condição: " + weather.Condicao;
-                umidadeLabel.Text = "Umidade: " + weather.Umidade;
-                velocidadeVentoLabel.Text = "Velocidade do Vento: " + weather.VelocidadeVento + " km/h";
-            }
-            else
-            {
-                await DisplayAlert("Erro", "Não foi possível obter os dados meteorológicos para esta cidade.", "OK");
-            }
-        }
-
-        private async Task<Weather> Teste(string cidade)
-        {
-            Weather weather = new();
-
-
-            if (!string.IsNullOrWhiteSpace(cidade))
-            {
-                try
-                {
-                    string responseBody = await HttpServiceRequest(cidade);
-
-                    // Faça o parse do responseBody conforme necessário e atualize sua UI
-                    weather = JsonConvert.DeserializeObject<Weather>(responseBody);
-                    return weather;
-                }
-                catch (Exception ex)
-                {
-                    await DisplayAlert("Erro de Requisição", ex.Message, "OK");
-                }
-            }
-            else
-            {
-                await DisplayAlert("Erro", "Por favor, insira o nome da cidade.", "OK");
-            }
-
-            return weather;
-        }
-
-        private async Task<string> HttpServiceRequest(string cidade)
-        {
-            HttpResponseMessage response = await _httpClient.GetAsync($"https://sua-api.com/previsao/{cidade}");
-            response.EnsureSuccessStatusCode(); // Lança uma exceção se o código de status não for bem-sucedido
-            string responseBody = await response.Content.ReadAsStringAsync();
-            return responseBody;
+            await DisplayAlert("Erro", "Não foi possível obter os dados meteorológicos para esta cidade.", "OK");
         }
     }
 
+    private async Task<Weather> RecuperarPrevisaoPorCidade(string cidade)
+    {
+        string responseBody = await HttpServiceRequest(cidade);
+        return JsonConvert.DeserializeObject<Weather>(responseBody);
+    }
+
+    private async Task<string> HttpServiceRequest(string cidade)
+    {
+        string responseBody = string.Empty;
+        if (!string.IsNullOrWhiteSpace(cidade))
+        {
+            try
+            {
+
+                HttpResponseMessage response = await _httpClient.GetAsync(URI(cidade));
+                response.EnsureSuccessStatusCode();
+                responseBody = await response.Content.ReadAsStringAsync();
+
+                return responseBody;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro de Requisição", ex.Message, "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("Erro", "Por favor, insira o nome da cidade.", "OK");
+        }
+
+        return responseBody;
+    }
+
+    private static string URI(string cidade)
+    {
+        return $"https://sua-api.com/previsao/{cidade}";
+    }
+
+    private void RecuperarCoordenadasDoIP()
+    {
+        var latitudePadrao = -22.740385;
+        var longitudePadrao = -43.024463;
+        var data = new
+        {
+            geoplugin_latitude = -22.740385,
+            geoplugin_longitude = -43.024463
+        };
+        string uri = "http://www.geoplugin.net/json.gp";
+
+        //se url retorno latidade e longitude não forem vazios entra no if
+        if (data.geoplugin_latitude == 0 && data.geoplugin_longitude == 0)
+        {
+            RecuperarLocalUsuario(latitudePadrao, longitudePadrao);
+        }
+        else
+        {
+            RecuperarLocalUsuario(data.geoplugin_latitude, data.geoplugin_longitude);
+        }
+    }
+
+    private void RecuperarLocalUsuario(double latitude, double longitude)
+    {
+
+    }
 }
